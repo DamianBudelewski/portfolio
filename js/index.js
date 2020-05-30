9ac43f5611b1956578d31f6ae9fdee4c6b63adb0
Damian Budelewski <budelewski.damian@gmail.com>
2020-05-30 22:00:47 +0200

    Customized with different html sections Added tools svg icons and JS toTop button

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link')
+const navLinks = document.querySelectorAll('.nav__link');
+const toTopButton = document.querySelector('.toTopBtn');
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open')
@@ -9,4 +10,17 @@ navLinks.forEach(link => {
     link.addEventListener('click', () => {
         document.body.classList.remove('nav-open');
     })
+})
+
+toTopButton.addEventListener('click', () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+})
+
+window.addEventListener('scroll', () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    toTopButton.style.display = "block";
+  } else {
+    toTopButton.style.display = "none";
+  }
 })
