433c0f64a316d91fa9860af2b761626071021877
Damian Budelewski <budelewski.damian@gmail.com>
2020-10-21 18:40:01 +0200

    Gatsby tutorial 3 out of 8

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
-
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <Header headerText="Hello Gatsby!" />
-      <p>What a world.</p>
-    </div>
+    <Layout>
+        <Link to="/contact/">Contact</Link>
+        <Header headerText="Hello Gatsby!" />
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+      <h1>Lorem ipsum</h1>
+      <p>
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
+      </p>
+      <h1>Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC</h1>
+      <p>
+        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        1914 translation by H. Rackham
+      </p>
+
+    </Layout>
   )
 }
